648cfb4022cdcd63cec6866f7d53d3c21f18bb8d
Taylor Piccarreto <tpic12@gmail.com>
2020-01-24 10:37:29 -0500

    first test

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const {expect} = require('chai').expect
+const supertest = require('supertest')
+const app = require('../src/app')
+
+describe('App', ()=>{
+  it('GET / responds 200 and Hello World', ()=>{
+    return supertest(app)
+    .get('/')
+    .expect(200, {message: 'Hello World'});
+  })
+})
